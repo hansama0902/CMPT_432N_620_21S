aa191e0f604bca2a76e1d1b295975697f3c4b900
hansama0902 <20091247@marist.edu>
2021-03-31 23:58:58 +0800

    Add parser log info
    Signed-off-by: hansama0902 <20091247@marist.edu>

--- project2/src/Parser.java
+++ project2/src/Parser.java
@@ -6,69 +6,72 @@ import java.util.Scanner;
 public class Parser {
   Tree CST = new Tree();
 
-
-  public void parse() {
-
+  public void LOG(String method) {
+    System.out.println("PARSER: " + method);
   }
 
-  public void parseProgram(ArrayList<Token> tokens) {
+  public void parse() {
+    LOG("parser()");
+  }
 
+  public void parseProgram() {
+    LOG("parserProgram()");
   }
 
   public void parseBlock() {
-
+    LOG("parseBlock()");
   }
 
   public void parseStatementList() {
-
+    LOG("parseStatementList()");
   }
 
   public void parseStatement() {
-
+    LOG("parseStatement()");
   }
 
   public void parsePrintStatement() {
-
+    LOG("parsePrintStatement()");
   }
 
   public void parseAssignmentStatement() {
-
+    LOG("parseAssignmentStatement()");
   }
 
   public void parseVarDecl() {
-
+    LOG("parseVarDecl()");
   }
 
   public void parseWhileStatement() {
-
+    LOG("parseWhileStatement()");
   }
 
   public void parseIfStatement() {
-
+    LOG("parseIfStatement()");
   }
 
   public void parseExpr() {
-
+    LOG("parseExpr()");
   }
 
   public void parseIntExpr() {
-
+    LOG("parseIntExpr()");
   }
 
   public void parseStringExpr() {
-
+    LOG("parseStringExpr()");
   }
 
   public void parseBooleanExpr() {
-
+    LOG("parseBooleanExpr()");
   }
 
   public void parseId() {
-
+    LOG("parseId()");
   }
 
   public void parseCharList() {
-
+    LOG("arseCharList()");
   }
 
   public void match(String type) {
